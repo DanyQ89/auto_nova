6306d02f38ebcbf02dbfa3b3698f841ed8d5759e
DanyQ89 <108182297+DanyQ89@users.noreply.github.com>
2025-02-27 17:34:59 +0300

    im back baby
    need to make search by number, fix adminka and make better design of main page

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,30 +1,118 @@
 // Получаем модальное окно
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("detailModal");
 
 // Получаем кнопку, которая открывает модальное окно
-var btn = document.getElementById("openModal");
+var btn = document.getElementById("addDetailBtn");
 
 // Получаем элемент <span>, который закрывает модальное окно
 var span = document.getElementsByClassName("close")[0];
 
 // Когда пользователь нажимает на кнопку, открывается модальное окно
-btn.onclick = function() {
-    modal.style.display = "block";
+if (btn) {
+    btn.onclick = function() {
+        openModal('add');
+    };
 }
 
 // Когда пользователь нажимает на <span> (x), закрывается модальное окно
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-// Когда пользователь нажимает в любом месте вне модального окна, оно закрывается
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+if (span) {
+    span.onclick = function() {
+        closeModal();
+    };
 }
 
 function toggleMenu() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('active');
 }
+// Когда пользователь нажимает в любом месте вне модального окна, оно закрывается
+window.onclick = function(event) {
+    const menu = document.getElementById('menu');
+    const hamburger = document.querySelector('.hamburger'); // Получаем элемент гамбургера
+
+    // Проверяем, был ли клик вне меню и вне гамбургера
+    if (event.target !== menu && event.target !== hamburger && menu.classList.contains('active')){
+        menu.classList.remove('active'); // Закрываем меню
+    } else if (event.target === modal) {
+        closeModal();
+    }
+};
+
+// Функция для открытия модального окна
+function openModal(action, detail = {}) {
+    const title = document.getElementById('modalTitle');
+    const detailId = document.getElementById('detailId');
+    const nameInput = document.getElementById('name');
+    const priceInput = document.getElementById('price');
+    const priceWDiscountInput = document.getElementById('price_w_discount');
+    const dateInput = document.getElementById('data_created');
+    const commentInput = document.getElementById('comment');
+    const origNumberInput = document.getElementById('orig_number');
+    const conditionInput = document.getElementById('condition');
+    const percentInput = document.getElementById('percent');
+    const cpkInput = document.getElementById('CpK');
+    const colorInput = document.getElementById('color');
+    const skladInput = document.getElementById('sklad');
+    const idDetailInput = document.getElementById('ID_detail');
+    const brandInput = document.getElementById('brand');
+    const modelAndYearInput = document.getElementById('model_and_year');
+    const submitButton = document.getElementById('submitButton');
+
+    if (action === 'edit') {
+        title.textContent = 'Редактировать деталь';
+        // Заполнение полей данными детали
+        detailId.value = detail.id;
+        skladInput.value = detail.sklad || '';
+        idDetailInput.value = detail.ID_detail || '';
+        brandInput.value = detail.brand || '';
+        modelAndYearInput.value = detail.model_and_year || '';
+        nameInput.value = detail.name || '';
+        priceInput.value = detail.price || '';
+        priceWDiscountInput.value = detail.price_w_discount || '';
+        dateInput.value = detail.data_created || '';
+        commentInput.value = detail.comment || '';
+        origNumberInput.value = detail.orig_number || '';
+        conditionInput.value = detail.condition || '';
+        percentInput.value = detail.percent || 0;
+        cpkInput.value = detail.CpK || '';
+        colorInput.value = detail.color || '';
+        submitButton.textContent = 'Обновить данные';
+    } else {
+        title.textContent = 'Добавить деталь';
+        // Очистка полей для добавления новой детали
+        detailId.value = '';
+        skladInput.value = '';
+        idDetailInput.value = '';
+        brandInput.value = '';
+        modelAndYearInput.value = '';
+        nameInput.value = '';
+        priceInput.value = '';
+        priceWDiscountInput.value = '';
+        dateInput.value = '';
+        commentInput.value = '';
+        origNumberInput.value = '';
+        conditionInput.value = '';
+        percentInput.value = 0;
+        cpkInput.value = '';
+        colorInput.value = '';
+        submitButton.textContent = 'Сохранить';
+    }
+    modal.style.display = 'block';
+}
+
+// Функция для закрытия модального окна
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+// Функция для редактирования детали
+function editDetail(detailId) {
+    fetch(`/update_detail/${detailId}`)
+        .then(response => response.json())
+        .then(detail => {
+            openModal('edit', detail);
+        })
+        .catch(error => console.error('Ошибка:', error));
+}
+
+// Функция для переключения меню
